fca10357191b7eb70f9c4ef78c32fa9d22078c14
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-12-15 22:05:34 +0100

    BackOffice_Register : same modification

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('title', "Register Page")
 
 @section('auth-form')
-<h1 class="mb-3">S'inscrire</h1>
+<h1 class="mb-3">Register</h1>
 <form action="{{ route('register') }}" method="POST">
     @csrf
     <div class="form-group">
@@ -31,9 +31,9 @@
         @enderror
     </div>
     <div class="form-group mb-0">
-        <button class="btn btn-primary btn-block" type="submit">S'inscrire</button>
+        <button class="btn btn-primary btn-block" type="submit">Register</button>
     </div>
 </form>
 
-<div class="text-center dont-have">Vous avez deja un compte? <a href="login.html">Se connecter</a> </div>
+<div class="text-center dont-have">Already have an account? <a href="login.html">Login</a> </div>
 @endsection
