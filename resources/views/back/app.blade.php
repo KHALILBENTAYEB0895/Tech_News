ab3281a18be1e93c589ffc67a12accd8d141f49e
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-12-28 16:26:47 +0100

    BUG FIXING : fix the bug result of poorly written comment

--- resources/views/back/app.blade.php
+++ resources/views/back/app.blade.php
@@ -7,23 +7,23 @@
       content="width=device-width, initial-scale=1.0, user-scalable=0"
     />
     <title>@yield('title')</title>
-    {# Dashboard - Links #}
+    {{-- {# Dashboard - Links #} --}}
     @include('back.partials.styles')
-    {# Fin Dashbord Link #}
+    {{-- {# Fin Dashbord Link #} --}}
   </head>
 
   <body>
-    {# Main wrapper #}
+    {{-- {# Main wrapper #} --}}
     <div class="main-wrapper">
-      {# Debut Header #}
+      {{-- {# Debut Header #} --}}
       @include('back.partials.header')
-      {# Fin Header #} 
-      {# ------------------ #}
-      {# Debut Sidebar #}
+      {{-- {# Fin Header #} 
+      {# ------------------ #} --}}
+      {{-- {# Debut Sidebar #} --}}
       @include('back.partials.sidebar')
       {# Fin Sidebar #} 
-      {# --------------------- #} 
-      {# Contenu de la page #}
+      {{-- {# --------------------- #}  --}}
+      {{-- {# Contenu de la page #} --}}
       <div class="page-wrapper">
         <div class="content container-fluid">
           <div class="page-header">
@@ -32,11 +32,11 @@
           @yield('dashboard-content')
         </div>
       </div>
-      {# Fin Contenu de la page #}
+      {{-- {# Fin Contenu de la page #} --}}
     </div>
-    {# Scripts dashboard #}
+    {{-- {# Scripts dashboard #} --}}
     @include('back.partials.scripts')
-    {# Fin Script Dashboard #}
+    {{-- {# Fin Script Dashboard #} --}}
 
     @if (session()->get('success'))
       <script>
